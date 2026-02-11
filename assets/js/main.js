5c79ad610bb48c3ee54de75d0525792207b942ae
roasx <132033098+roasx@users.noreply.github.com>
2026-02-11 19:35:43 +0000

    added js function for h2

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,17 @@
 // main.js
 
-console.log("Hello, World!");
+let targetElement = document.querySelector("h2");
+console.log(targetElement.outerHTML);
+
+targetElement.addEventListener("mouseover", function() {
+    console.log(this);
+    this.innerText = "TIME";
+    this.style = "cursor: wait";
+});
+
+console.log(this.innerText);
+
+targetElement.addEventListener("mouseout", function() {
+    this.innerText = "IKEA";
+    this.style = "cursor: pointer";
+});
